ef09cd87ea0d5588a25a544d9e29269d3c110774
akash7233 <akashyadav15805@gmail.com>
2024-03-12 21:53:59 +0530

    Tried to Deploy

--- client/src/utils/ApiFetch.js
+++ client/src/utils/ApiFetch.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const Base_url = 'http://localhost:8000/api/v1'
+const Base_url = 'https://connectify-pied.vercel.app/api/v1/' || 'http://localhost:8000/api/v1'
 
 const axiosInstance = axios.create({
     baseURL: Base_url,
