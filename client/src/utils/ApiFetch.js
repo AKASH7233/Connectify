363cbbf7436284e30fbfc91534d5166994ef7fa5
akash7233 <akashyadav15805@gmail.com>
2024-03-12 23:30:09 +0530

    update base_url  to deploy

--- client/src/utils/ApiFetch.js
+++ client/src/utils/ApiFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const Base_url = "https://connectify-app-tau.vercel.app"
+const Base_url = "https://connectify-app-tau.vercel.app/api/v1"
 
 const axiosInstance = axios.create({
     baseURL: Base_url,
