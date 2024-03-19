9f0989a76c4d2593f1dc511366a25d59e870eb61
akash7233 <akashyadav15805@gmail.com>
2024-03-19 15:55:37 +0530

    Removed props from myprofile.jsx

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import axiosInstance from '../utils/ApiFetch'
-import profileImg from '../assets/profile.png'
-import toast from 'react-hot-toast'
-import { useNavigate } from 'react-router-dom'
 import ProfileHeader from '../components/ProfileHeader'
-import { useDispatch, useSelector } from 'react-redux'
 
 function MyProfile() {
-    const navigate = useNavigate()
-    const user = useSelector(state => state?.auth?.user).user
-    console.log(user);
+
   return (
     <div className='bg-black text-white'>
-        <ProfileHeader username={user?.username} name={user?.fullName} followers={user?.followers || 0 } following={user?.following || 0} profileImg={user?.ProfileImage} bio = {user?.bio || ""}/>
+        <ProfileHeader/>
     </div>
   )
 }
