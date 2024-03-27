56031a7b3a3db53e6c3935343cf9c1e0d63394d2
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-03-28 03:09:44 +0530

    chat model and routing

---
+++ backend/src/controllers/ChatController.js
@@ -0,0 +1,24 @@
+import ChatModel from "../models/ChatModel";
+import { ApiError } from "../utils/ApiError";
+
+export const createChat = async (req, res,next) => {
+    const newChat = new ChatModel({
+        member: [req.body.senderId, req.body.receiverId]
+    })
+    try {
+        const chat = await newChat.save();
+    } catch (error) {
+        return next(new ApiError(error.message, 500))
+    }
+}
+
+export const userChats = async (req, res,next) => {
+    try {
+        const chat = await ChatModel.find({
+            member: { $in:[req.params.userId]}
+        })
+        res.status(200).json(chat)
+    } catch (error) {
+        return next(new ApiError(error.message, 500))
+    }
+}
