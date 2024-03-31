66b9d026bff8ed8e2c8747acc1e28d063dd25f09
akash7233 <akashyadav15805@gmail.com>
2024-03-31 18:50:59 +0530

    added followlist

--- client/src/components/Post/FollowerLists/Follow.jsx
+++ client/src/components/Post/FollowerLists/Follow.jsx
@@ -2,31 +2,41 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Followers, Following } from '../../../redux/followSlice'
 import { getUserData } from '../../../redux/authSlice'
-import { following } from '../../../../../backend/src/controllers/follow.controller'
 import Header from '../Header'
 import UserHeader from '../../userHeader/UserHeader'
+import { useParams } from 'react-router-dom'
 
 
-function Follow({userId}) {
+function Follow() {
     const dispatch = useDispatch()
-    const [users,SetUsers] = useState(null)
-
+    const {type} = useParams()
+    const {userId} = useParams()
+    const [user,setUser] = useState(null)
+    const [listType,setListType] = useState(type)
+    const otherType = listType == 'Followers' ? 'Following' : 'Followers'
+    const [otherlistType,setOtherListType] = useState(listType)
     const load = async() => {
-      let response = await dispatch(Followers(userId))
-      SetUsers(response?.payload?.data)
-      console.log(response);
+      let response ;
+      {listType == 'Followers' ? response = await dispatch(Followers(userId)) : response = await dispatch(Following(userId))}
+      setUser(response?.payload?.data)
     }
-    console.log(users);
-
+    console.log(user);
     useEffect(()=>{
       load()
-    },[])
+    },[listType])
   return (
-    <div>
-      {users?.map((user)=>( 
-        <UserHeader key={user?._id} user={user.followers} userId={user?._id}/>
-      ))}
+    <>
+      <div className='flex justify-evenly items-center py-4 bg-black text-white border-b'>
+      <button className={`bg-gray-400 px-10 py-2`} onClick={()=>{setListType(listType)}}>{listType}</button>
+      <h2 className='text-2xl opacity-15'>|</h2>
+      <button className='px-4 py-2' onClick={()=>{setListType(otherType)}}>{otherType}</button>
     </div>
+    <div className='min-h-[100vh] bg-black'>
+      {user?.map((user)=>( 
+        <UserHeader key={user?._id} user={user.followers || user.followings}/>
+      ))}
+  </div>
+    </>
   )
 }
 
