ff8cc173391571071034442b019c3c87da930705
akash7233 <akashyadav15805@gmail.com>
2024-04-27 20:10:42 +0530

    changed post's image section to view images through swipe for mobile view

--- client/src/components/Post/Header.jsx
+++ client/src/components/Post/Header.jsx
@@ -5,8 +5,10 @@ import profileImage from '../../assets/profile.png'
 import { useDispatch, useSelector } from 'react-redux';
 import { profile } from '../../redux/usersSlice';
 import { toggleFollow } from '../../redux/followSlice';
+import { IoArrowBackOutline, IoArrowForward } from "react-icons/io5";
 
 function Header({post}) {
+  console.log(post);
     const currentUser = useSelector(state=> state?.auth?.user) 
     const currentUserId = currentUser?.user?._id || currentUser[0]?._id
     const profileImg = post?.owner.ProfileImage ? post?.owner.ProfileImage : profileImage
@@ -34,7 +36,7 @@ function Header({post}) {
       postAt = `${diffMins}m ago`
     }
     else{
-      postAt `${diffMs * 60}s ago`
+      postAt = `${diffMs * 60}s ago`
     }
 
     const load = async() =>{
@@ -50,6 +52,7 @@ function Header({post}) {
       await dispatch(toggleFollow(post?.owner?._id))
       load()
     }
+    console.log(post?.postFile);
   return (
     <div>
         <div className=' text-white flex justify-between rounded-t py-2 mb-2 px-4 '>
@@ -64,8 +67,15 @@ function Header({post}) {
             </Link>
             <button className={` ${userId ? 'invisible' : 'block' } bg-blue-500 rounded  text-white px-5 text-md`} onClick={fetch} >{isFollowing?'Unfollow': 'Follow'}</button>
          </div>
-         <div className='h-64 px-4 w-full'>
-            <img src={post?.postFile} alt="postImg" className='h-full w-[100vw] rounded-xl object-fill'/>
+         <div className='h-64 px-4'>
+            {Array.isArray(post?.postFile) && 
+              <div className='relative h-64  flex whitespace-nowrap scroll overflow-y-auto '>{ post?.postFile?.map((img)=>(
+                <img src={img} alt="postImg" className='h-64 w-[100vw] rounded-xl border-2 border-black  object-fill'/>
+              ))}
+              {/* <div><button className='p-2 absolute top-[45%] left-1 rounded-3xl border ' ><IoArrowBackOutline  className='text-xl text-gray-400 hover:text-white'/></button><button className='p-2 absolute top-[46%] right-1 rounded-3xl border ' ><IoArrowForward  className='text-xl text-gray-400 hover:text-white'/></button></div> */}
+              </div>
+            }
+            {!Array.isArray(post?.postFile) && <img src={post?.postFile} alt="postImg" className='h-full w-[100vw]  rounded-xl object-fill'/>}
         </div>
         <h2 onClick={()=>{setReadMore(prev => !prev)}} className={`text-gray-400 mt-2 ml-5 w-[80vw] ${readMore ? 'clip' : 'truncate'} break-words`}>{post?.title}</h2>
     </div>
