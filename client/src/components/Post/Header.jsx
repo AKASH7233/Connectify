48d4f3b2ff834b304aaee605d3ff9956636899db
akash7233 <akashyadav15805@gmail.com>
2024-05-02 20:55:44 +0530

    changed ui of follow btn in post

--- client/src/components/Post/Header.jsx
+++ client/src/components/Post/Header.jsx
@@ -6,15 +6,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { profile } from '../../redux/usersSlice';
 import { toggleFollow } from '../../redux/followSlice';
 import { IoArrowBackOutline, IoArrowForward } from "react-icons/io5";
+import { BsThreeDots } from "react-icons/bs";
 
 function Header({post}) {
-  console.log(post);
     const currentUser = useSelector(state=> state?.auth?.user) 
     const currentUserId = currentUser?.user?._id || currentUser[0]?._id
     const profileImg = post?.owner.ProfileImage ? post?.owner.ProfileImage : profileImage
     const dispatch = useDispatch()
     const [isFollowing,setIsFollowing] = useState()
-    const userId = currentUserId == post?.owner?._id ? 'myprofile' : `user/${post?.owner?._id}`
+    const selfID = currentUserId == post?.owner?._id
+    const userId = selfID ? 'myprofile' : `user/${post?.owner?._id}`
     
     
     const [readMore,setReadMore] = useState(false)
@@ -52,10 +53,9 @@ function Header({post}) {
       await dispatch(toggleFollow(post?.owner?._id))
       load()
     }
-    console.log(post?.postFile);
   return (
     <div>
-        <div className=' text-white flex justify-between rounded-t py-2 mb-2 px-4 '>
+        <div className=' text-white flex justify-between items-center rounded-t py-2 mb-2 px-4 '>
             <Link to={`/${userId}`}>
                 <div className='flex gap-x-3 items-center'>
                 <img src={profileImg} alt="post_file" className='w-10 h-10 rounded-[50%] object-fit '/>
@@ -65,14 +65,22 @@ function Header({post}) {
                 </div>
                 </div>
             </Link>
-            <button className={` ${userId ? 'invisible' : 'block' } bg-blue-500 rounded  text-white px-5 text-md`} onClick={fetch} >{isFollowing?'Unfollow': 'Follow'}</button>
+            <div>
+            <button className={`${!selfID ? 'bg-gray-900 bg-opacity-90 border-2 border-gray-700 text-sm py-2 text-white w-24  rounded-xl' : 'invisible'}` } onClick={fetch} >{isFollowing?'Following': 'Follow'}</button>
+            </div>
          </div>
          <div className='h-64 px-4'>
             {Array.isArray(post?.postFile) && 
-              <div className='relative h-64  flex whitespace-nowrap scroll overflow-y-auto '>{ post?.postFile?.map((img)=>(
-                <img src={img} alt="postImg" className='h-64 w-[100vw] rounded-xl border-2 border-black  object-fill'/>
-              ))}
-              {/* <div><button className='p-2 absolute top-[45%] left-1 rounded-3xl border ' ><IoArrowBackOutline  className='text-xl text-gray-400 hover:text-white'/></button><button className='p-2 absolute top-[46%] right-1 rounded-3xl border ' ><IoArrowForward  className='text-xl text-gray-400 hover:text-white'/></button></div> */}
+              <div className='relative h-64  flex whitespace-nowrap scroll overflow-y-auto '>
+                { post?.postFile?.map((img,i)=>(
+                 <img src={img} key={i} alt="postImg" className='h-64 w-[100vw] rounded-xl border-2 border-black  object-fill'/>         
+                  ))
+                }
+                <div>
+                  <button className='p-2 fixed top-[45%] left-0 rounded-3xl border ' ><IoArrowBackOutline  className='text-xl text-black hover:text-white'/></button>
+                  <button className='p-2 fixed top-[46%] right-1 rounded-3xl border ' ><IoArrowForward  className='text-xl text-gray-400 hover:text-white'/></button>
+                  <button><BsThreeDots className='text-white'/></button>
+                </div>
               </div>
             }
             {!Array.isArray(post?.postFile) && <img src={post?.postFile} alt="postImg" className='h-full w-[100vw]  rounded-xl object-fill'/>}
