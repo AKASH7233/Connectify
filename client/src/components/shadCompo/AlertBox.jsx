e5023b11c6c3c697db5711856f31674e1d41198d
akash7233 <akashyadav15805@gmail.com>
2024-05-16 15:12:33 +0530

    added deleteAccount or deletePost option

--- client/src/components/shadCompo/AlertBox.jsx
+++ client/src/components/shadCompo/AlertBox.jsx
@@ -10,21 +10,32 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
+import { useDispatch } from 'react-redux'
+import { deletePost } from '@/redux/postSlice'
+import { deleteUser } from '@/redux/authSlice'
+import { useNavigate } from 'react-router-dom'
   
-function AlertBox({warning,open,resFunc}) {
+function AlertBox({warning,open,resFunc,id}) {
   
     const [show,setShow] = useState(open)
-    
-    const sentres = (e) => {
-      e.target.id == "cancel" ? resFunc(false) : resFunc(true)
-      setShow(false)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
+
+    const deleteAPost = async() =>{
+      await dispatch(deletePost(id))
+      window.history.go(-1)
+    }
+
+    const deleteCurrentuser = async() =>{
+      await dispatch(deleteUser())
+      navigate('/login')
     }
   return (
     <div>
         <AlertDialog open={show}>
             
-            <AlertDialogContent className='bg-black w-[90vw]'>
+            <AlertDialogContent className='bg-black w-[90vw] rounded-xl'>
                 <AlertDialogHeader>
                 <AlertDialogTitle className={'text-white'}>Are you absolutely sure?</AlertDialogTitle>
                 <AlertDialogDescription className='text-gray-400'>
@@ -33,8 +44,8 @@ function AlertBox({warning,open,resFunc}) {
                 </AlertDialogDescription>
                 </AlertDialogHeader> 
                 <AlertDialogFooter>
-                <AlertDialogCancel className='bg-slate-800 text-red-400' id='cancel' onClick={sentres}>Cancel</AlertDialogCancel>
-                <AlertDialogAction className='bg-slate-800 text-red-400' id='delete' onClick={sentres}>Delete</AlertDialogAction>
+                <AlertDialogCancel className='bg-slate-800  text-gray-400' id='cancel'>Cancel</AlertDialogCancel>
+                <AlertDialogAction className='bg-slate-800 text-red-400 border border-red-400' id='delete' onClick={warning == 'post ' ? deleteAPost : deleteCurrentuser}>Delete</AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
