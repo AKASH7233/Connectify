285d1957326a914e36294a9f7dde746da3bb785c
akash7233 <akashyadav15805@gmail.com>
2024-05-19 22:06:46 +0530

    added the new shareBtn icons in the share

--- client/src/components/shadCompo/Share.jsx
+++ client/src/components/shadCompo/Share.jsx
@@ -16,58 +16,71 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useRef, useState } from "react";
 import { useCallback } from "react";
+import ShareBtn from "../ShareBtn/ShareBtn";
 
-export function DialogCloseButton({link,drawer = false}) {
+export function DialogCloseButton({post,drawer = false}) {
 
-    const shareLink = `localhost:5173/viewpost/${link}/comment`
-    const [title,setTitle] = useState(false)
+    const shareLink = `localhost:5173/viewpost/${post?._id}/comment`
+    const title = post?.title
     const shareRef = useRef(null)
+    const [isCopied,setIsCopied] = useState(false)
 
     const copy = useCallback(() => {
         shareRef.current?.select()
         shareRef.current?.setSelectionRange(0, 999);
         window.navigator.clipboard.writeText(shareLink)
-        setTitle(true)
+        setIsCopied(true)
     })
 
+    
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">{drawer ? <span className="flex text-lg items-center gap-x-4"><Forward/> share</span>:<Forward/>}</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-md w-[90vw] rounded-xl bg-[#09090B] text-gray-400">
-        <DialogHeader>
-          <DialogTitle className='text-white'>Share link</DialogTitle>
-          <DialogDescription className='text-gray-400'>
-            Anyone who has this link will be able to view this.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex items-center space-x-2">
-          <div className="grid flex-1 gap-2">
-            <Label htmlFor="link" className="sr-only">
-              Link
-            </Label>
-            <Input
-              ref={shareRef}
-              id="link"
-              defaultValue = {shareLink}
-              readOnly
-              className='border-white  text-white rounded-[10px]'
-            />
-          </div>
-          <button onClick={copy} size="sm" className="p-3 bg-white rounded-[10px] text-black">
-            <MdContentCopy className="h-4 w-4 " />
-          </button>
-          {title && <p className="absolute right-3 top-[61%] text-sm bg-white px-2 text-black rounded-xl">copied</p>}
-      </div>
-        <DialogFooter className="sm:justify-start">
-          <DialogClose asChild>
-            <Button type="button" className='bg-white text-black mt-5 rounded-xl' variant="secondary">
-              Close
-            </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <div>
+     {/* {
+        !navigator.share &&  */}
+        <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">{drawer ? <span className="flex text-lg items-center gap-x-4"><Forward/> share</span>:<Forward/>}</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md w-[90vw] rounded-xl bg-[#09090B] text-gray-400">
+          <DialogHeader>
+            <DialogTitle className='text-white'>Share link</DialogTitle>
+            <DialogDescription className='text-gray-400'>
+              Anyone who has this link will be able to view this.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex items-center space-x-2">
+            <div className="grid flex-1 gap-2">
+              <Label htmlFor="link" className="sr-only">
+                Link
+              </Label>
+              <Input
+                ref={shareRef}
+                id="link"
+                defaultValue = {shareLink}
+                readOnly
+                className='border-white  text-white rounded-[10px]'
+              />
+            </div>
+            <button onClick={copy} size="sm" className="p-3 bg-white rounded-[10px] text-black">
+              <MdContentCopy className="h-4 w-4 " />
+            </button>
+            {/* {isCopied && <p className="lg:top-12 absolute right-3 top-[61%] text-sm bg-white px-2 text-black rounded-xl">copied</p>} */}
+        </div>
+        <div className="lg:px-6">
+          <ShareBtn title={title} url={shareLink}/>
+        </div>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" className='bg-white text-black mt-5 rounded-xl' variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+     {/* } */}
+     
+    </div>
   )
 }
