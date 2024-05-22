8df2342a78f2374bc2677bdfe5e90567bbea19e5
akash7233 <akashyadav15805@gmail.com>
2024-05-22 17:32:16 +0530

    added a new menulist for follows

--- client/src/components/Feed/MenuBar.jsx
+++ client/src/components/Feed/MenuBar.jsx
@@ -5,11 +5,14 @@ import { IoCloudUploadOutline } from "react-icons/io5";
 import { CgLogOut } from "react-icons/cg";
 import { IoMdSettings } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
+import { FaUserFriends } from "react-icons/fa";
 import icon from "../../assets/icon.jpg" 
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function MenuBar({ActiveMenuItem}) {
-
+  const currentUser = useSelector(state=> state?.auth?.user) 
+  const currentUserId = currentUser?.user?._id || currentUser[0]?._id
   ActiveMenuItem != undefined ? localStorage.setItem('item',JSON.stringify(ActiveMenuItem)) : ''
 
   let respItem = ActiveMenuItem || JSON.parse(localStorage.getItem('item'))
@@ -21,6 +24,7 @@ function MenuBar({ActiveMenuItem}) {
             <Link to={'/search'}><button className={`flex items-center justify-left gap-x-10 mx-2 ${respItem == 'Search' ? 'bg-gray-900 bg-opacity-90 border-gray-700' : ''} rounded-xl p-2 w-72 md:w-52  hover:bg-gray-900 hover:bg-opacity-90 border-2 border-black text-white hover:border-gray-700`}><FaSearch className='text-lg'/><span>Search</span></button></Link>
             <Link to={'/uploadpost'}><button className={`flex items-center justify-left gap-x-10 mx-2 ${respItem == 'Upload' ? 'bg-gray-900 bg-opacity-90 border-gray-700' : ''} rounded-xl p-2 w-72 md:w-52 hover:bg-gray-900 hover:bg-opacity-90 border-2 border-black text-white hover:border-gray-700`}><IoCloudUploadOutline className='text-lg '/><span>Upload Post</span></button></Link>
             <Link to={'/myProfile'}><button className={`flex items-center justify-left gap-x-10 mx-2 ${respItem == 'Profile' ? 'bg-gray-900 bg-opacity-90 border-gray-700' : ''} rounded-xl p-2 w-72 md:w-52 hover:bg-gray-900 hover:bg-opacity-90 border-2 border-black text-white hover:border-gray-700`}><CgProfile className='text-lg'/><span>Profile</span></button></Link>
+            <Link to={`/followlist/Followers/${currentUserId}`}><button className={`flex items-center justify-left gap-x-10 mx-2 ${respItem == 'Follow' ? 'bg-gray-900 bg-opacity-90 border-gray-700' : ''} rounded-xl p-2 w-72 md:w-52 hover:bg-gray-900 hover:bg-opacity-90 border-2 border-black text-white hover:border-gray-700`}><FaUserFriends className='text-lg'/><span>Follows</span></button></Link>
             <button className={`flex items-center justify-left gap-x-10 mx-2 ${respItem == '' ? 'bg-gray-900 bg-opacity-90 border-gray-700' : ''} rounded-xl p-2  hover:bg-gray-900 hover:bg-opacity-90 border-2 border-black text-white hover:border-gray-700`}><IoMdSettings className='text-lg'/><span>Settings</span></button>
             <button className='flex items-center justify-left gap-x-10 mx-2  rounded-xl p-2 hover:bg-gray-900 hover:bg-opacity-90 border-2 border-black text-red-400 hover:border-red-400'><CgLogOut className='text-lg'/><span>Logout</span></button>
         </div>
