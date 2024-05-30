fec905e42281a7d2716dbb7254ff20025364208c
akash7233 <akashyadav15805@gmail.com>
2024-05-30 21:23:45 +0530

    created a page that contain stories compo

---
+++ client/src/components/Blinks/Blink.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Stories from './Zucks'
+function Blink() {
+  
+  return (
+    <div className='bg-black px-3 py-2'>
+      <Stories />
+    </div>
+  )
+}
+
+export default Blink
