2a39dabb027f3d5fbe32704e8cc14b1aee3d30a5
akash7233 <akashyadav15805@gmail.com>
2024-06-01 23:59:22 +0530

    updated the ui of uploadpost component

--- client/src/components/UploadPost/UploadPost.jsx
+++ client/src/components/UploadPost/UploadPost.jsx
@@ -11,7 +11,7 @@ function UploadPost() {
     let [fileName,setFileName] = useState([])
     const [post,setPost] = useState({
         title: '',
-        postFile: ''
+        postFile: []
     })
     const eventHandler = (e) => {
         let value = e.target.files;
@@ -30,16 +30,11 @@ function UploadPost() {
     console.log(post);
 
     const remove = (e) =>{
-        console.log(e.target.id);
-
-        let find = Array.from(post?.postFile)
-        console.log(find);
-        let files = []
-        find?.find((post)=>{
-               post.name != e.target.id ? files.push(post) : ''
-        })
-        console.log(files);
-        setPost({...post,[e.target.id] : files})
+        const fileId = e.target.id;
+        if (Array.isArray(post.postFile)) {
+            const updatedPostFiles = post.postFile.filter(file => file.name !== fileId);
+            setPost({ ...post, postFile: updatedPostFiles });
+        }
         
         // console.log(names);
         // post?.postFile?.map((post)=>{
@@ -49,8 +44,9 @@ function UploadPost() {
         // console.log(name);
         // setFileName(name)
         // setPost({...post,[e.target.name]: e.target.files})        
-
     }
+
+    console.log(post.postFile);
     
     const onFormSubmit = async(e) => {
         e.preventDefault()
@@ -73,16 +69,26 @@ function UploadPost() {
             // navigate('/')
         }
     }
+
+    const [className,setClassName] = useState()
+
+    const handleRemoveClick = (e) => {
+        if (e.target.tagName === 'H2') {
+            setClassName(e.target.id);
+            remove(e);
+        }
+    };
+
   return (
-    <div className='w-[100vw] h-[100vh] flex items-center align-center  bg-[#000000] overflow-hidden' method='post'>
-        <div className='bg-gray-800 py-12 mx-10 bg-opacity-50 rounded-xl text-white text-center'>
+    <div className='w-full h-[100vh] flex items-center align-center  bg-[#000000] overflow-hidden' method='post'>
+        <div className='bg-gray-800 lg:mx-20 py-12 mx-10 bg-opacity-50 rounded-xl text-white text-center'>
             <h2 className='text-lg'>Upload the Pictures</h2>
             <p className='text-gray-400 text-sm'>in JPEG,PNG or WEBP</p>
         <form onSubmit={onFormSubmit} encType='multipart/form-data'>
         <div className='my-7'>
         <label 
             htmlFor="file"
-            className=' rounded-xl px-9 my-4 py-2 text-white bg-[#7e11f3]'
+            className='bg-gray-900 bg-opacity-90 border-2 border-gray-700 text-sm py-3 px-5 text-white w-24  rounded-xl'
         >
             + Add the Picture
         </label>
@@ -98,11 +104,11 @@ function UploadPost() {
             />
             
         </div>
-        <div className='text-left text-sm mx-4 my-4 mb-6'>
+        <div className='text-left text-sm mx-4 my-4 mb-6' onClick={handleRemoveClick}>
             {fileName?.map((name,i)=>(
-                <div className='flex justify-between items-center my-2'>
-                    <h2 key={i} className='clip'>{name}</h2>
-                    <h2 onClick={remove} id={name}>X</h2>
+                <div key={i} id={name} className={`flex justify-between items-center my-2 ${className == name ? 'hidden' : ''}`} onClick={handleRemoveClick}>
+                    <p key={i} className='clip'>{name}</p>
+                    <h2 onClick={remove} className='cursor-pointer' id={name}>X</h2>
                 </div>
             ))}
         </div>
@@ -116,7 +122,7 @@ function UploadPost() {
         placeholder='write about the post . . . .'
         className=' w-full px-5 text-gray-400 bg-transparent outline-none'/>
         <div>
-        <button type="submit" className='text-white bg-[#7e11f3] px-20 mx-4 py-3 rounded-xl'>Upload Post</button>
+        <button type="submit" className='bg-gray-900 bg-opacity-90 border-2 border-gray-700 text-sm py-4 px-5 text-white rounded-xl'>Upload Post</button>
         </div>
         </form>
         </div>  
