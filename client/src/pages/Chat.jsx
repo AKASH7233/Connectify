1eb17a51bbe92985a13acba1cd3df27621f2806d
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-06-18 03:32:50 +0530

    feat: Add PersonList component for displaying a list of people
    The PersonList component is responsible for rendering a list of people with their profile images, usernames, and online status. It also includes click handlers for selecting a person and closing the list. This component improves the user experience by providing a visually appealing and interactive way to view and select people.
    
    Co-authored-by: [Damodar Yadav]

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,4 +1,7 @@
+import { Camera, GroupIcon, PodcastIcon, SendIcon, SettingsIcon, User2Icon } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
+import { IoCall, IoImage, IoVideocam } from "react-icons/io5";
+import { MdOutlineReport } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { useMediaQuery } from 'react-responsive';
 import { useLocation } from "react-router-dom";
@@ -10,9 +13,6 @@ import { io } from "socket.io-client";
 import { SheetSide } from "@/components/Drawer";
 import { fetchChatId, fetchPerson } from "@/redux/chatSlice";
 import { fetchMessages, sendMessageDispatch } from "@/redux/messageSlice";
-import { Camera, GroupIcon, PodcastIcon, SendIcon, SettingsIcon, User2Icon } from "lucide-react";
-import { IoCall, IoImage, IoVideocam } from "react-icons/io5";
-import { MdOutlineReport } from "react-icons/md";
 
 function ChatApp() {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
@@ -65,12 +65,16 @@ function ChatApp() {
                 setMessages(oldMessage);
             }
             const result = (await dispatch(fetchPerson(user?._id))).payload;
-            setPersonData(result);
+            const updatedPersonData = result.map(person => ({
+                ...person,
+                isOnline: onlineUsers.includes(person._id)
+            }));
+            setPersonData(updatedPersonData);
         };
 
         loadChatId();
         setLoading(false);
-    }, [person, user, dispatch]);
+    }, [person, user, dispatch, onlineUsers]);
 
     const personClickHandler = (index) => {
         setPerson(personData[index]);
@@ -127,101 +131,107 @@ function ChatApp() {
                 console.error('A error occurred while reading the file.');
             };
         }
+        
     };
 return (
     loading ?
         <Loader color="#00BFFF" height={300} width={300} /> :
-        <div className={`bg-dark-200 h-screen flex ${isMobile ? 'flex-col' : 'flex-row'}`}>
+        <div className={`h-screen flex flex-col md:flex-row bg-gradient-to-r from-gray-600 to-black`}>
 
-        {/* Sidebar */}
-        <div className={`flex ${isMobile ? 'w-full' : 'w-1/4'} bg-gray-200`}>
+            {/* Sidebar */}
+            <div className={`hidden md:flex md:w-1/5 bg-black bg-gradient-to-b from-[#2d3436] to-black shadow-lg`}>
 
-            {/* Icons */}
-            <div className="w-1/4 flex flex-col items-center py-4 space-y-5">
-                <User2Icon className="w-8 text-blue-700" />
-                <GroupIcon className="w-8 text-blue-700" />
-                <SettingsIcon className="w-8 text-blue-700" />
-                <Camera className="w-8 text-blue-700" />
-                <PodcastIcon className="w-8 text-blue-700" />
+                {/* Icons */}
+                <div className="hidden md:flex md:flex-col items-center md:py-4 md:space-y-5 space-x-5 md:space-x-0 md:w-1/4">
+                    <User2Icon className="w-6 text-gray-400" />
+                    <GroupIcon className="w-6 text-gray-400" />
+                    <SettingsIcon className="w-6 text-gray-400" />
+                    <Camera className="w-6 text-gray-400" />
+                    <PodcastIcon className="w-6 text-gray-400" />
+                </div>
+
+                {/* People to Chat */}
+                <aside className="w-full bg-gradient-to-b from-slate-700 to-black p-6">
+                    {/* People to Chat code here... */}
+                    <h2 className="text-xl font-bold mb-4 text-white">Message</h2>
+                    <ul className="space-y-4 overflow-y-auto ">
+                        {personData?.map((member, index) => (
+                            <li key={index} onClick={() => personClickHandler(index)} className="flex items-center justify-between cursor-pointer hover:bg-gray-900 p-3 rounded shadow-lg">
+                                <span className="flex items-center">
+                                    {member.ProfileImage ? <img src={member.ProfileImage} className="mr-2 w-8 h-8 rounded-3xl object-fill" alt="user's ProfileImg" /> : <User2Icon className="mr-2 w-8 text-gray-400" />}
+                                    <span className="text-white">{member.username}</span>
+                                    {member.isOnline ?
+                                        <span className="ml-2 bg-green-500 rounded-full w-3 h-3 inline-block"></span> :
+                                        <span className="ml-2 bg-red-500 rounded-full w-3 h-3 inline-block"></span>
+                                    }
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </aside>
             </div>
 
-            {/* People to Chat */}
-            <aside className="w-full bg-gray-300 p-6">
-                <h2 className="text-xl font-bold mb-4">Message</h2>
-                <ul className="space-y-4 overflow-y-auto ">
-                    {personData?.map((member, index) => (
-                        <li key={index} onClick={() => personClickHandler(index)} className="flex items-center justify-between cursor-pointer hover:bg-gray-400 p-3 rounded">
-                            <span className="flex items-center">
-                                {member.ProfileImage ? <img src={member.ProfileImage} className="mr-2 w-8 h-8 rounded-3xl object-fill" alt="user's ProfileImg" /> : <User2Icon className="mr-2 w-8 text-blue-700" />}
-                                <span>{member.username}</span>
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </aside>
-        </div>
-
-        {/* Main Chat Area */}
-        {person ? (
-            <div className={`w-full bg-gray-200 h-screen flex flex-col justify-between relative ${isMobile ? 'mt-4' : ''}`}>
-
-                {/* Header */}
-                <header className="w-[22rem] absolute top-0 left-1/2 transform -translate-x-1/2 bg-blue-800 text-white py-4 px-6 items-center shadow-md flex justify-between rounded-full my-5">
-                    <div className="flex items-center justify-start">
-                        {person?.ProfileImage ? <SheetSide icon={<img src={person?.ProfileImage} className="mr-2 w-8 h-8 rounded-3xl object-fill-" alt="user's ProfileImg"/>} /> : <SheetSide icon={<User2Icon className="hover:text-blue-600 transition duration-200 w-8" />} /> }
-                        
-                        <div className="flex-col">
-                            <h2 className="text-xl font-bold">{person.fullName}</h2>
-                            <p className="text-sm">Active now</p>
+            {/* Main Chat Area */}
+            {person ? (
+                <div className={`w-full md:w-4/5 bg-gradient-to-r from-gray-800 to-gray-900 h-screen flex flex-col justify-between relative mt-4 md:mt-0 shadow-lg`}>
+                    {/* Header */}
+                    <header className="w-full md:w-[22rem] absolute top-0 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700 text-white py-4 px-6 items-center shadow-md flex justify-between rounded-full my-5">
+                        <div className="flex items-center justify-start">
+                            {person?.ProfileImage ? <SheetSide icon={<img src={person?.ProfileImage} className="mr-2 w-8 h-8 rounded-3xl object-fill-" alt="user's ProfileImg" />} /> : <SheetSide icon={<User2Icon className="hover:text-gray-600 transition duration-200 w-8" />} />}
+                            <div className="flex-col">
+                                <h2 className="text-xl font-bold">{person.fullName}</h2>
+                                <p className="text-sm">{onlineUsers.includes(person.id) ? 'Active now' : 'Offline'}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex space-x-5">
-                        <IoCall className="text-3xl hover:text-blue-600 transition duration-200" />
-                        <IoVideocam className="text-3xl hover:text-blue-600 transition duration-200" />
-                        <MdOutlineReport className="text-3xl hover:text-blue-600 transition duration-200" />
-                    </div>
-                </header>
+                        <div className="flex space-x-5">
+                            <IoCall className="text-3xl hover:text-gray-600 transition duration-200" />
+                            <IoVideocam className="text-3xl hover:text-gray-600 transition duration-200" />
+                            <MdOutlineReport className="text-3xl hover:text-gray-600 transition duration-200" />
+                        </div>
+                    </header>
 
-                {/* Main Chat Area */}
-                <main ref={chatContainerRef} className="w-full flex-1 p-6 overflow-y-auto space-y-4">
-                    {messages.map((message, i) =>
-                        <animated.div style={props} key={i} className={`w-fit rounded-xl py-3 px-5 break-words ${user?._id == messages[i].senderId ? 'bg-blue-800 text-white ml-auto' : 'bg-gray-400 text-gray-800 mr-auto'}`}>
-                            {message?.message}
-                        </animated.div>
-                    )}
-                </main>
+                    {/* Main Chat Area */}
+                    <main ref={chatContainerRef} className="w-full flex-1 p-6 overflow-y-auto space-y-4 bg-gradient-to-l from-black via-gray-700 to-gray-900">
+                        {messages.map((message, i) =>
+                            <animated.div style={props} key={i} className={`w-fit rounded-xl py-3 px-5 break-words text-black ${user?._id == messages[i].senderId ? 'bg-indigo-100 ml-auto '  : ' bg-blue-200  mr-auto'}`}>
+                                {message?.message}
+                            </animated.div>
+                        )}
+                    </main>
 
-                {/* Message Input Area */}
-                <footer className="bg-white py-5 px-7 flex items-center shadow-md">
-                    <div className="flex items-center border border-gray-400 rounded-full px-5 py-3 focus-within:border-blue-600 w-full ">
+                    {/* Message Input Area */}
+                    <footer className="bg-gradient-to-l from-gray-900 via-gray-800 to-gray-700 py-5 px-7 flex items-center shadow-md">
+                        <div className="flex items-center border border-gray-700 rounded-full px-5 py-3 focus-within:border-gray-600 w-full ">
 
-                        {/* Attachments */}
-                        <label htmlFor="image_upload">
-                            <IoImage className="text-blue-700 text-3xl cursor-pointer hover:text-blue-800 transition duration-200" />
-                        </label>
-                        <input
-                            className="hidden"
-                            type="file"
-                            onChange={handleImageUpload}
-                            id="image_upload"
-                            name="image_upload"
-                            accept=".jpg , .jpeg, .png , .svg"
-                        />
+                            {/* Attachments */}
+                            <label htmlFor="image_upload">
+                                <IoImage className="text-gray-400 text-3xl cursor-pointer hover:text-gray-500 transition duration-200" />
+                            </label>
+                            <input
+                                className="hidden"
+                                type="file"
+                                onChange={handleImageUpload}
+                                id="image_upload"
+                                name="image_upload"
+                                accept=".jpg , .jpeg, .png , .svg"
+                            />
 
-                        <input type="text" value={message} onChange={(event) => setMessage(event.target.value)} placeholder="Type your message..." className="ml-3 focus:outline-none w-full" />
-                        <button onClick={sendMessage} className="ml-5 bg-blue-800 text-white px-5 py-3 rounded-full hover:bg-blue-900 focus:outline-none">
-                            <SendIcon />
-                        </button>
-                    </div>
-                </footer>
-            </div>
-        ) : (
-            <div className="w-full h-full flex items-center justify-center">
-                <h2 className="text-3xl">Select a person to chat with</h2>
-            </div>
-        )}
+                            <input type="text" value={message} onChange={(event) => setMessage(event.target.value)} placeholder="Type your message..." className="ml-3 focus:outline-none w-full text-gray-300 bg-gray-800" />
+                            <button onClick={sendMessage} className="ml-5 bg-gray-700 text-white px-5 py-3 rounded-full hover:bg-gray-600 focus:outline-none">
+                                <SendIcon />
+                            </button>
+                        </div>
+                    </footer>
+                </div>
+            ) : (
+                <div className="w-full h-full flex items-center justify-center">
+                    <h2 className="text-3xl text-white">Select a person to chat with</h2>
+                </div>
+            )}
 
-    </div>
+        </div>
 );
 }
 export default ChatApp;
+
+// completly change the color/theme of ui from light and blue to dark and gray
