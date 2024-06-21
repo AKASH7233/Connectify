2947bb4843ea139e0a13bfba9dae91c5a6e62bdb
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-06-21 20:28:01 +0530

    duplicate user

--- client/src/utils/Socket/index.js
+++ client/src/utils/Socket/index.js
@@ -12,20 +12,17 @@ io.on('connection', socket => {
         if (!activeUsers.some(user => user.userId === userId)) {
             activeUsers.push({ userId, socketId: socket.id });
         }
-        console.log('User connected', activeUsers);
         io.emit('getUsers', activeUsers);
     });
 
     socket.on('disconnect', () => {
         activeUsers = activeUsers.filter(user => user.socketId !== socket.id);
-        console.log('User disconnected', activeUsers);
         io.emit('getUsers', activeUsers);
     });
 
     socket.on('sendMessage', (data) => {
         const { receiverId } = data;
         const user = activeUsers.find(user => user.userId === receiverId);
-        console.log('Sending message from socket to ', receiverId);
         console.log('Data :', data);
         if (user) {
             io.to(user.socketId).emit('receiveMessage', data);
