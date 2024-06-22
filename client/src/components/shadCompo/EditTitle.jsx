78528f022297ebbe58be1232b1c1bb7ef9956a9f
akash7233 <akashyadav15805@gmail.com>
2024-06-22 17:18:14 +0530

    written the code of editTitle so that it can be used in both editPostTitle and editComment

--- client/src/components/shadCompo/EditTitle.jsx
+++ client/src/components/shadCompo/EditTitle.jsx
@@ -16,22 +16,33 @@ import { MdEdit } from "react-icons/md"
 import toast from "react-hot-toast"
 import { EditPost } from "@/redux/postSlice"
 import { DialogClose } from "@radix-ui/react-dialog"
+import { editComment } from "@/redux/commentSlice"
 
-export function EditTitle({post}) {
+export function EditTitle({post = null , comment = null }) {
 
     const dispatch = useDispatch()
-    const [title,setTitle] = useState(post?.title)
+    const [title,setTitle] = useState(post?.title) 
+    const [commentTitle,setCommentTitle] = useState(comment?.comment)
+    console.log(commentTitle);
     const inputHandler = (e) => {
         setTitle(e.target.value)
     }
 
     const submit = async(e) =>{
         e.preventDefault()
-        if(title?.trim() == ''){
+        if(post){
+          if(title?.trim() == ''){
             toast.error(`Title is required !`)
             return null
+          }
+          await dispatch(EditPost({id: post?._id,title :title}))
+        } else{
+              if(commentTitle?.trim() == ''){
+                toast.error(`Title is required !`)
+                return null
+              }
+              await dispatch(editComment({url: comment?._id, comment: commentTitle}))
         }
-         await dispatch(EditPost({id: post?._id,title :title}))
          location.reload()
     }
   return (
@@ -39,7 +50,7 @@ export function EditTitle({post}) {
         <DialogTrigger className='text-md flex items-center gap-x-4 my-2 '><span><MdEdit className='text-xl'/></span>Edit</DialogTrigger>
       <DialogContent className="sm:max-w-[425px] bg-[#09090B] text-white w-[90vw] rounded-xl">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Edit {`${post ? "Post's Title": "Comment"}`}</DialogTitle>
           {/* <p className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">X</p> */}
           <DialogDescription className= 'text-gray-400'>
             Make changes to your profile here. Click save when you're done.
@@ -48,14 +59,22 @@ export function EditTitle({post}) {
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-1">
             <Label htmlFor="name" className="text-left">
-              New Title
+              New {post ? 'Title': 'Comment'}
             </Label>
-            <Input
+            {
+              post ?
+              <Input
               id="fullName"
               defaultValue={title}
               className="col-span-3 rounded-xl"
               onChange = {inputHandler}
-            />
+              /> : <Input
+              id="fullName"
+              defaultValue={commentTitle}
+              className="col-span-3 rounded-xl"
+              onChange = {(e)=>{setCommentTitle(e.target.value)}}
+              />
+            }
           </div>
         </div>
         <DialogFooter>
