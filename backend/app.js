1cc0b8d4b4d46bdb12b88bd356a6ddf51faeb33f
akash7233 <akashyadav15805@gmail.com>
2024-06-23 21:42:37 +0530

    changed the cors url for deployment

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import  cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: "https://connectify-h4nsgcbe5-akash7233s-projects.vercel.app",
+    origin: "*",
     credentials: true
 }))
 app.use(cookieParser())
