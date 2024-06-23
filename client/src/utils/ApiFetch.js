e75fdfabd6ffb8352b1972cd71bb62dc7737019d
akash7233 <akashyadav15805@gmail.com>
2024-06-23 22:39:41 +0530

    changed the base url for deployment

--- client/src/utils/ApiFetch.js
+++ client/src/utils/ApiFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const Base_url = "https://connectify-6tvs.onrender.com" 
+const Base_url = "https://connectify-6tvs.onrender.com/api/v1" 
 
 const axiosInstance = axios.create({
     baseURL: Base_url,
