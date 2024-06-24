fefe3f59efc39b89b7baace76374f63834936736
Akash Yadav <70443620+AKASH7233@users.noreply.github.com>
2024-06-24 12:34:55 +0530

    changed cors origin for testing

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import  cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: "https://connectiify-xi.vercel.app" //|| "http://localhost:5173",
+    origin:  "http://localhost:5173",
     ,
     credentials: true
 }))
