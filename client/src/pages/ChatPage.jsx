2e73055a7e7075aedec8c33eccf501215e7bd245
akash7233 <akashyadav15805@gmail.com>
2024-06-27 16:49:46 +0530

    resolved chat page not rendering properly

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -10,25 +10,14 @@ const ChatPage = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
     console.log(isMobile);
     return (
-        <div className="h-[90vh]">
+        <div>
             {
-                !isMobile && 
-                <div className="flex gap-x-3 relative lg:px-40 md:px-3 bg-gray-800 overflow-x-hidden text-white"> 
-                    <div className=" lg:w-[20vw] lg:block xl:block md:block fixed md:w-[30vw] sm:w-[10vw] hidden"><MenuBar /></div>
-                    <div className="lg:w-[20vw] md:w-[30vw] sm:w-[10vw]"></div>
-                    <div className="lg:w-[30vw] md:w-[45vw]"><ChatApp/></div>
-                    <div className="  relative">
-                        <div className="w-[20vw] z-40 hidden md:fixed md:block lg:fixed lg:block"><UpcomingUpdates/></div>
-                    </div>
-                </div>
-            }
-            {
-                isMobile &&
-                <div>
+                isMobile ? <div>
                     <Navbar/>
-                        <ChatApp/>
+                    <ChatApp/>
                     <Footer/>
-                </div>
+                    </div>
+                :   <ChatApp />
             }
         </div>
     )
