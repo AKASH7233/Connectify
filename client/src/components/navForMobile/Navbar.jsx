f9307e55419fdc68af3d5ab11fe13b423433c381
akash7233 <akashyadav15805@gmail.com>
2024-06-28 13:46:39 +0530

    resolved white space showing at the top of blink on minute scroll

--- client/src/components/navForMobile/Navbar.jsx
+++ client/src/components/navForMobile/Navbar.jsx
@@ -5,6 +5,7 @@ import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 const Navbar = () => {
     
   const [show, setShow] = useState(true);
+  const [AtTop, setAtTop] = useState(true);
   let lastScrollY = window.scrollY;
 
   const controlNavbar = () => {
@@ -16,18 +17,29 @@ const Navbar = () => {
     lastScrollY = window.scrollY;
   };
 
+  const controlAtTop = () => {
+    if (window.scrollY < 100) {
+      setAtTop(true);
+    } else {
+      setAtTop(false);
+    }
+  };
+
   useEffect(() => {
     window.addEventListener('scroll', controlNavbar);
+    window.addEventListener('scroll', controlAtTop);
     return () => {
       window.removeEventListener('scroll', controlNavbar);
+      window.addEventListener('scroll', controlAtTop);
     };
   }, []);
 
 
+
   return (
-   <div className='mb-[53px]'>
+   <div className={`${AtTop && show ? 'mb-[52px]' : '' }`}>
      <nav className={`fixed  top-0 left-0 w-full z-40 bg-gray-950 shadow-md transition-transform duration-300 ${show ? 'transform translate-y-0' : 'transform -translate-y-full'}`}>
-      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+      <div className="container mx-auto px-4 py-3 flex justify-between  items-center">
         <Link to={'/'}><div className="text-xl  text-white font-bold" onClick={()=>{window.scrollTo({top: 0 , behavior: 'smooth'})}}>ConnectiFy</div></Link>
         <Link to={'/chat'}><div className="text-xl  text-white font-bold"><IoChatbubbleEllipsesOutline fontSize={24}/></div></Link>
       </div>
